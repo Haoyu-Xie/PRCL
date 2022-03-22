371ece700154367967fdacab00383bfdb60eb0d6
Wang Changqi <79964633+Richard-Wang-2499@users.noreply.github.com>
2022-03-22 10:18:33 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import math
+
+a = math.erfc(3.288)
+print(a)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
