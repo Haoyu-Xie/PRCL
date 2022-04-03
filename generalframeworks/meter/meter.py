c6c63d668413470acbbb073ff79391518a6f504a
Wang Changqi <79964633+Richard-Wang-2499@users.noreply.github.com>
2022-04-03 15:40:02 +0800

    Add files via upload

--- generalframeworks/meter/meter.py
+++ generalframeworks/meter/meter.py
@@ -64,14 +64,49 @@ class AverageValueMeter(Meter):
             if c != 0:
                 Dice_dct['DSC_{}'.format(c)] = np.array([self.value(i, mode='all') for i in range(len(self.dice))])[:, c].mean()
         return Dice_dct
-            
 
-         
-            
+class ConfMatrix(Meter):
+    def __init__(self, num_classes):
+        super(ConfMatrix, self).__init__()
+        self.num_classes = num_classes
+        self.mat = None
+        self.reset()
+        self.mIOU = []
+        self.Acc = []
+
+    def add(self, pred_logits, label):
+        pred_logits = pred_logits.argmax(1).flatten()
+        label = label.flatten()
+        n = self.num_classes
+        if self.mat is None:
+            self.mat = torch.zeros((n, n), dtype=torch.int64, device=pred_logits.device)
+        with torch.no_grad():
+            k = (label >= 0) & (label < n)
+            inds = n * label[k].to(torch.int64) + pred_logits[k]
+            self.mat += torch.bincount(inds, minlength=n ** 2).reshape(n, n)
 
 
+    def value(self, mode='mean'):
+        h = self.mat.float()
+        self.acc = torch.diag(h).sum() / h.sum()
+        self.iu = torch.diag(h) / (h.sum(1) + h.sum(0) - torch.diag(h))
+        if mode == 'mean':
+            return torch.mean(self.iu).item(), self.acc.item()
+        else:
+            raise ValueError("mode must be in (mean)")
 
-
+    def reset(self):
+        self.mIOU = []
+        self.Acc = []
+    
+    def summary(self) -> dict:
+        mIOU_dct: dict = {}
+        Acc_dct: dict = {}
+        for c in range(self.num_classes):
+            if c != 0:
+                mIOU_dct['mIOU_{}'.format(c)] = np.array([self.value(i, mode='all')[0] for i in range(len(self.mIOU))])[:, c].mean()
+                Acc_dct['Acc_{}'.format(c)] = np.array([self.value(i, mode='all')[1] for i in range(len(self.mIOU))])[:, c].mean()
+        return mIOU_dct, Acc_dct
 
 def compute_dice(mask1: torch.Tensor, mask2: torch.Tensor) -> torch.Tensor:
 
