babe02a86eb5b75d29b2bc8a7698c8052fd7c1f7
Wang Changqi <wangchangqi98@Gmail.com>
2022-10-23 15:14:52 +0800

    Update batch_train.sh

--- script/batch_train.sh
+++ script/batch_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./script/command.sh prcl_92  8 1  "python -u prcl_res100.py --config ./config/VOC_prcl_config_92_0.yaml"
+./script/command.sh prcl_183  8 1  "python -u prcl_res100.py --config ./config/VOC_prcl_config_183_0.yaml"
 
 
 
